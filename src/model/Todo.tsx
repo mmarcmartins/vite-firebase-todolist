f57e5f8efdf1c84cdc0466e6ec0d1ad0c0aa5865
Marcelo Martins <mmarcmartins@gmail.com>
2022-02-22 22:50:45 -0300

    First commit

---
+++ src/model/Todo.tsx
@@ -0,0 +1,25 @@
+export default class Todo {
+    #id : string;
+    #text: string;
+    #isComplete: boolean;
+    
+    constructor(id: string, text: string, isComplete: boolean){
+        this.#id = id;
+        this.#text = text;
+        this.#isComplete = isComplete;
+    }
+
+    get id(){
+        return this.#id;
+    }
+    get text(){
+        return this.#text;
+    }
+    get isComplete(){
+        return this.#isComplete;
+    }
+
+    static toModel(objTodo: Todo){
+        return new Todo(objTodo.id, objTodo.text, objTodo.isComplete)
+    }
+}
